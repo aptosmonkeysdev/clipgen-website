d5571638ce3ed27bfc7ea72be975209ba987a9a8
Charbel Eid <charbeltonyeid@gmail.com>
2025-11-13 14:19:24 +0200

    Changed sizes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,9 @@ import GreyGradientText from "./GreyGradientText";
 export default function Title({ orangeTitle, whiteGradient, greyGradient }) {
   return (
     <>
-      <div className="text-sm font-semibold text-orange">{orangeTitle}</div>
+      <div className="text-md font-semibold text-orange">{orangeTitle}</div>
 
-      <h2 className="text-3xl font-bold bg-gradient-to-b from-white via-white to-[#999999] bg-clip-text text-transparent">
+      <h2 className="text-5xl leading-tight font-bold bg-gradient-to-b from-white via-white to-[#999999] bg-clip-text text-transparent">
         {whiteGradient}
       </h2>
       <GreyGradientText greyGradient={greyGradient} />
